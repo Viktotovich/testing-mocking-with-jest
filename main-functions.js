87ba2b7b21670e5fd39fa55df2a59e85a71cc841
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-10-30 02:13:23 +0400

    "Wrote down next steps for testing"

--- main-functions.js
+++ main-functions.js
@@ -5,3 +5,24 @@ const functionController = {
 };
 
 export { functionController };
+
+/* 
+https://medium.com/javascript-scene/mocking-is-a-code-smell-944a70c90a6a
+
+God damnit, another day, another rabbit hole. Generators 
+are a must have for declarative code that avoids mocking. Also, apparently mocking is bad
+
+Also, integration tests
+
+Next steps:
+1 - https://jestjs.io/docs/setup-teardown
+
+2 - Generators?
+https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/async%20%26%20performance/ch4.md
+
+3 - Pick-up BST back up with testing up my sleeves
+
+or 
+
+3 - Proceed with BattleShip first
+*/
